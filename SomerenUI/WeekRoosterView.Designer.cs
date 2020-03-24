ed22d4b06c5fff437414fc426642349c8682c8a8
Unknown <JunkmailBelith@gmail.com>
2020-03-24 10:07:06 +0100

    BTWView regional issue solved + start Opdracht4VersieC

---
+++ SomerenUI/WeekRoosterView.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace SomerenUI
+{
+    partial class WeekRoosterView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.gridViewMon = new System.Windows.Forms.DataGridView();
+            this.columnActivityName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.counsellorNamesColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gridViewTue = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gridViewWed = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gridViewThu = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gridViewFri = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewMon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewTue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewWed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewThu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewFri)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Week Rooster";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SomerenUI.Properties.Resources.someren;
+            this.pictureBox1.Location = new System.Drawing.Point(825, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 110);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // gridViewMon
+            // 
+            this.gridViewMon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridViewMon.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.columnActivityName,
+            this.counsellorNamesColumn});
+            this.gridViewMon.Location = new System.Drawing.Point(172, 53);
+            this.gridViewMon.Name = "gridViewMon";
+            this.gridViewMon.Size = new System.Drawing.Size(241, 163);
+            this.gridViewMon.TabIndex = 9;
+            // 
+            // columnActivityName
+            // 
+            this.columnActivityName.HeaderText = "Activiteit";
+            this.columnActivityName.Name = "columnActivityName";
+            // 
+            // counsellorNamesColumn
+            // 
+            this.counsellorNamesColumn.HeaderText = "Begeleiding";
+            this.counsellorNamesColumn.Name = "counsellorNamesColumn";
+            // 
+            // gridViewTue
+            // 
+            this.gridViewTue.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridViewTue.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.gridViewTue.Location = new System.Drawing.Point(485, 53);
+            this.gridViewTue.Name = "gridViewTue";
+            this.gridViewTue.Size = new System.Drawing.Size(241, 163);
+            this.gridViewTue.TabIndex = 10;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Activiteit";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Begeleiding";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // gridViewWed
+            // 
+            this.gridViewWed.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridViewWed.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.gridViewWed.Location = new System.Drawing.Point(41, 251);
+            this.gridViewWed.Name = "gridViewWed";
+            this.gridViewWed.Size = new System.Drawing.Size(241, 163);
+            this.gridViewWed.TabIndex = 11;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "Activiteit";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "Begeleiding";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // gridViewThu
+            // 
+            this.gridViewThu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridViewThu.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6});
+            this.gridViewThu.Location = new System.Drawing.Point(330, 251);
+            this.gridViewThu.Name = "gridViewThu";
+            this.gridViewThu.Size = new System.Drawing.Size(241, 163);
+            this.gridViewThu.TabIndex = 12;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.HeaderText = "Activiteit";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.HeaderText = "Begeleiding";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            // 
+            // gridViewFri
+            // 
+            this.gridViewFri.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridViewFri.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn7,
+            this.dataGridViewTextBoxColumn8});
+            this.gridViewFri.Location = new System.Drawing.Point(621, 251);
+            this.gridViewFri.Name = "gridViewFri";
+            this.gridViewFri.Size = new System.Drawing.Size(241, 163);
+            this.gridViewFri.TabIndex = 13;
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.HeaderText = "Activiteit";
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            // 
+            // dataGridViewTextBoxColumn8
+            // 
+            this.dataGridViewTextBoxColumn8.HeaderText = "Begeleiding";
+            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
+            // 
+            // WeekRoosterView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gridViewFri);
+            this.Controls.Add(this.gridViewThu);
+            this.Controls.Add(this.gridViewWed);
+            this.Controls.Add(this.gridViewTue);
+            this.Controls.Add(this.gridViewMon);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "WeekRoosterView";
+            this.Size = new System.Drawing.Size(938, 455);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewMon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewTue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewWed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewThu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewFri)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView gridViewMon;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnActivityName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn counsellorNamesColumn;
+        private System.Windows.Forms.DataGridView gridViewTue;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridView gridViewWed;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridView gridViewThu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridView gridViewFri;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
+    }
+}
